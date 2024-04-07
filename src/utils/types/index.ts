8fe1e99da20ceef69227cd8480696df577382a49
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-04-07 19:46:14 -0300

    feat: create util global types

---
+++ src/utils/types/index.ts
@@ -0,0 +1,16 @@
+type Tweet = {
+  tweetText: string;
+  tweetAuthorName: string;
+  tweetCreatedAt: string;
+};
+
+type User = {
+  email: string;
+  name: string;
+  points: number;
+};
+
+type Campaign = {
+  id: string;
+  campaignPhrase: string;
+};
