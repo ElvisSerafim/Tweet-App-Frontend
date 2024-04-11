f8bb71af19f644d25f0510c3a5d5c56ea5e7eac0
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-04-11 17:42:55 -0300

    feat: add create tweet action on Post

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,23 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import InputTweet from "../InputTweet";
 import { Button } from "@nextui-org/react";
-import Image from "next/image";
+import moment from "moment";
 
-const Tweet = () => {
+const Tweet = ({ createTweet }: TweetProps) => {
+  const [tweetMessage, setTweetMessage] = useState<string>("");
+
+  const generateTweet = () => {
+    const tweetData: TweetRequest = {
+      message: tweetMessage,
+      userEmail: "elvisserafim6@gmail.com",
+      createdAt: moment().format("YYYY-MM-DD HH:mm:ss"),
+    };
+
+    createTweet(tweetData);
+    setTweetMessage("");
+  };
   return (
-    <>
-      <div className="flex flex-col items-end gap-4 border-solid border-1 p-4 rounded-md">
-        <InputTweet />
-        <div className="flex w-full flex-row items-center text-white justify-between">
-          <div className="flex flex-row gap-2">
-            <p className="text-sm">
-              Create tweets using the campaign phrase for make points!
-            </p>
-          </div>
-          <Button color="primary">Post</Button>
+    <div className="flex flex-col items-end gap-4 border-solid border-1 p-4 rounded-md">
+      <InputTweet
+        tweetMessage={tweetMessage}
+        setTweetMessage={setTweetMessage}
+      />
+      <div className="flex w-full flex-row items-center text-white justify-between">
+        <div className="flex flex-row gap-2">
+          <p className="text-sm">
+            Create tweets using the campaign phrase for make points!
+          </p>
         </div>
+        <Button onClick={generateTweet} color="primary">
+          Post
+        </Button>
       </div>
-    </>
+    </div>
   );
 };
 
