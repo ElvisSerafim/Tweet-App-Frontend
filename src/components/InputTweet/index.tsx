2a8249bd19d28001aad3f103dd54381853a1746d
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-04-11 17:39:00 -0300

    feat: add tweet mensage props

--- src/components/InputTweet/index.tsx
+++ src/components/InputTweet/index.tsx
@@ -3,20 +3,20 @@
 import { Textarea } from "@nextui-org/react";
 import React from "react";
 
-const InputTweet = () => {
+const InputTweet = ({ tweetMessage, setTweetMessage }: InputTweetProps) => {
   return (
-    <>
-      <Textarea
-        variant="underlined"
-        color="primary"
-        radius="sm"
-        placeholder="What are you thinking ?"
-        fullWidth
-        classNames={{
-          input: "text-white text-base",
-        }}
-      />
-    </>
+    <Textarea
+      variant="underlined"
+      color="primary"
+      radius="sm"
+      placeholder="What are you thinking ?"
+      fullWidth
+      value={tweetMessage}
+      onValueChange={setTweetMessage}
+      classNames={{
+        input: "text-white text-base",
+      }}
+    />
   );
 };
 
