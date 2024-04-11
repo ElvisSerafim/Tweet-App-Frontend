a66523ae8785f93d819219765ef8e01803750b19
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-04-11 17:37:23 -0300

    feat: integration signin form with react hook form

--- src/components/SigninForm/index.tsx
+++ src/components/SigninForm/index.tsx
@@ -1,24 +1,57 @@
+import {
+  createUserSignInData,
+  createUserSignInSchema,
+} from "@/validators/userSignValidator";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Input } from "@nextui-org/react";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 const SigninForm = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<createUserSignInData>({
+    resolver: zodResolver(createUserSignInSchema),
+  });
+
+  const onSubmit = (data: createUserSignInData) => console.log(data);
+
   return (
-    <form className="flex flex-col justify-between h-[250px] gap-4">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col justify-between h-[250px] gap-4"
+    >
       <div className="flex flex-col gap-4">
-        <Input
-          isRequired
-          label="Email"
-          placeholder="Enter your email"
-          type="email"
-          variant="bordered"
-        />
-        <Input
-          isRequired
-          label="Password"
-          placeholder="Enter your password"
-          type="password"
-          variant="bordered"
-        />
+        <div>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => <Input {...field} />}
+          />
+          <Input
+            isRequired
+            label="Email"
+            placeholder="Enter your email"
+            type="email"
+            variant="bordered"
+            {...register("email")}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <div>
+          <Input
+            isRequired
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            variant="bordered"
+            {...register("password")}
+          />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
       </div>
       <div className="flex gap-2 justify-end">
         <Button fullWidth className="pointer" color="primary">
