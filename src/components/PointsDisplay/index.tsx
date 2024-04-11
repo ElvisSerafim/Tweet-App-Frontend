931fe5809d18086da1148f51c2cdfe724eabc1ed
Elvis Serafim <elvis.serafim@deliverit.com.br>
2024-04-11 17:43:20 -0300

    feat: add points props value

--- src/components/PointsDisplay/index.tsx
+++ src/components/PointsDisplay/index.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
 
-const PointsDisplay = () => {
+const PointsDisplay = ({ points }: PointsDisplayProps) => {
   return (
-    <>
-      <div className="flex flex-row gap-2 text-center items-center">
-        <p>10</p>
-        <Image
-          src={"/star-icon.svg"}
-          width={15}
-          height={15}
-          alt={"star-icon"}
-        />
-      </div>
-    </>
+    <div className="flex flex-row gap-2 text-center items-center">
+      <p>{points}</p>
+      <Image src={"/star-icon.svg"} width={15} height={15} alt={"star-icon"} />
+    </div>
   );
 };
 
